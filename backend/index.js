8413bb56778206c804d9cae73e1548f26042ba00
mayank9370 <mayank701130@gmail.com>
2025-07-15 18:47:36 +0530

    adding

--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
 
+
+// Root route handler
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
